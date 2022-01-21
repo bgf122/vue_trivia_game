68cbe7de3f96d94005bac74d6cd953418c607125
Nico Behnen <Azruim@live.fi>
2022-01-21 16:10:02 +0200

    feat: removed unnecessary events and updated difficulty list with final values.

--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -1,4 +1,8 @@
 <script setup>
+
+// TODO
+// Call API request
+
 import { reactive, ref } from "vue";
 const username = ref("");
 const questions = ref(0);
@@ -6,60 +10,52 @@ const difficulty = ref("");
 const category = ref("");
 
 const difficultiesList = reactive([
-    "easy", "medium", "hard"
+    "Any Difficulty", "Easy", "Medium", "Hard"
 ]);
 const categoriesList = reactive([
-    "sports", "science", "history"
+    "Any Category", "sports", "science", "history"
+    // TODO 
+    // Populate from API
 ]);
-
-const onDifficultyChange = event => {
-    console.log(event.target.value);
-}
-const onCategoryChange = event => {
-    console.log(event.target.value);
-}
 
 const onSubmitClick = event => {
     console.log("Clicked submit");
+    // TODO
+    // Save to store
 }
 </script>
 
 <template>
-  <main>
-    <h1>Start Page</h1>
-    <form @submit.prevent="onSubmit">
-      <fieldset>
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="username" />
-      </fieldset>
+    <main>
+        <h1>Start Page</h1>
+        <form @submit.prevent="onSubmit">
+            <fieldset>
+                <label for="username">Username</label>
+                <input type="text" id="username" v-model="username" />
+            </fieldset>
 
-      <fieldset>
-        <select @change="onDifficultyChange">
-            <option value="">Choose difficulty</option>
-            <option v-for="item in difficultiesList" :key="item">
-                {{ item }}
-            </option>
-        </select>
-      </fieldset>
+            <fieldset>
+                <label for="questions">Number of questions</label>
+                <input type="number" id="questions" v-model="questions" />
+            </fieldset>
 
-      <fieldset>
-        <label for="questions">Number of questions</label>
-        <input type="number" id="questions" v-model="questions" />
-      </fieldset>
+            <fieldset>
+                <select>
+                    <option value>Choose difficulty</option>
+                    <option v-for="item in difficultiesList" :key="item">{{ item }}</option>
+                </select>
+            </fieldset>
 
-      <fieldset>
-        <select @change="onCategoryChange">
-            <option value="">Choose category</option>
-            <option v-for="item in categoriesList" :key="item">
-                {{ item }}
-            </option>
-        </select>
-      </fieldset>
+            <fieldset>
+                <select>
+                    <option value>Choose category</option>
+                    <option v-for="item in categoriesList" :key="item">{{ item }}</option>
+                </select>
+            </fieldset>
 
-        <button @click="onSubmitClick">Submit</button>
-
-    </form>
-  </main>
+            <button @click="onSubmitClick">Start</button>
+        </form>
+    </main>
 </template>
 
 <style scoped>
