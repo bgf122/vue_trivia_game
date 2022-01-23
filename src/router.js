3fce76b7e0d05d3e79de7cfadb09d32e8a268cf9
Nico Behnen <Azruim@live.fi>
2022-01-23 18:34:11 +0200

    feat: added routes and states.

--- src/router.js
+++ src/router.js
@@ -1,11 +1,22 @@
-import { createRouter, createWebHistory } from "vue-router"
-import StartScreen from './views/StartScreen.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Start from "./views/Start.vue"
+import Trivia from "./views/Trivia.vue"
+import Results from "./views/Results.vue"
 
 const routes = [
     {
         path: "/",
-        component: StartScreen 
-}]
+        component: Start
+    },
+    {
+        path: "/trivia",
+        component: Trivia
+    },
+    {
+        path: "/results",
+        component: Results
+    }
+]
 
 export default createRouter({
     history: createWebHistory(),
--- src/store.js
+++ src/store.js
@@ -2,10 +2,24 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-
+        username: "",
+        amount: 10,
+        category: 10,
+        difficulty: "easy",
     },
     mutations: {
-        
+        setUsername: (state, username) => {
+            state.username = username;
+        },
+        setAmount: (state, amount) => {
+            state.amount = amount;
+        },
+        setCategory: (state, category) => {
+            state.category = category;
+        },
+        setDifficulty: (state, difficulty) => {
+            state.difficulty = difficulty;
+        },
     },
     actions: {
         
