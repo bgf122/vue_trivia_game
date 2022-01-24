3c530a87669a5dca63a802dabf94d7baf2f05b7a
Nico Behnen <Azruim@live.fi>
2022-01-24 11:51:06 +0200

    feat: added token api request and saved the token to store from startform.

--- src/api/trivia.js
+++ src/api/trivia.js
@@ -1,19 +1,18 @@
 import { TRIVIA_BASE_URL } from "./";
 
-export async function apiGetTriviaQuestions(amount=1, category="", difficulty="") {
+export async function apiGetTriviaQuestions(amount = 1, category = "", difficulty = "") {
     try {
         const response = await fetch(`${TRIVIA_BASE_URL}amount=${amount}&category=${category}&difficulty=${difficulty}`);
-        
-        if(!response.ok) {
+
+        if (!response.ok) {
             throw new Error("Could not fetch questions")
         }
 
         const { results } = await response.json()
-        
-        return [ null, results]
+
+        return [null, results]
 
     } catch (error) {
-        return [ error.message, [] ]
+        return [error.message, []]
     }
-    
 }
